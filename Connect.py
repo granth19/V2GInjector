5f37b4ddcaedc108a5c739f6f969685b0c58e35c
Grant Hewett <granth19@vt.edu>
2022-06-15 13:04:19 -0400

    It's not worth it to get threading working

--- Connect.py
+++ Connect.py
@@ -1,30 +1,17 @@
 import os
 import binascii
 import code
-import multiprocessing
 from core.Network import *
-
-def NetworkSniff():
-    #Network().sniff(eth1)
-    Network().pcap('PEV.pcap')
 
 
 #while(len(Network().hpgp) == 0):
 #    Network().sniff(eth1)
 Network().pcap('PEV.pcap')
+for key in Network().hpgp:
+    newDict = Network().hpgp[key]
+    NMK = newDict['NMK']
+#NMK = bytes(NMK).decode('UTF8','replace')
+NMK = binascii.hexlify(NMK)
+#print('plctool -M -K', NMK.decode() + ' local')
+os.system('plctool -M -K ' + NMK.decode() + ' local')
 
-if __name__ == "__main__":
-    p1 = multiprocessing.Process(target=NetworkSniff)
-    p1.start()
-    while True:
-        if len(Network().hpgp) != 0:
-            p1.terminate()
-            p1.join()
-            for key in Network().hpgp:
-                newDict = Network().hpgp[key]
-                NMK = newDict['NMK']
-            #NMK = bytes(NMK).decode('UTF8','replace')
-            NMK = binascii.hexlify(NMK)
-            print('plctool -M -K', NMK.decode())
-            #os.system('plctool -M -K ' + NMK.decode()+ 'local')
-            break
